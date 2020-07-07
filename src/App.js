98c250725f4992feb500f63e314adf08a15e94f9
alvalle09 <phredvalle@gmail.com>
2020-07-07 00:45:43 -0500

    add material-ui, disable add button when input empty

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Button } from '@material-ui/core';
 import "./App.css";
 
 
@@ -18,7 +19,9 @@ function App() {
     <h1>My Todo App ⛏</h1>
     <form>
       <input value={input} onChange={event => setInput(event.target.value)} />
-      <button type="submit" onClick={addTodo}>Add Todo</button>
+      <Button disabled={!input} type="submit" onClick={addTodo} variant="contained" color="primary">
+         Add Todo
+      </Button>
     </form>
     
     <ul>
